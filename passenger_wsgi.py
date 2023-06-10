4fcd799e1b299b3b4b9ba3a6ca9b8eae1374877c
Dániel Mizsák <mizsak.dani@gmail.com>
2023-06-10 23:04:51 +0200

    Change pytest to tox in GitHub actions

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,12 @@ import imp
 import os
 import sys
 
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.extend(
+    [
+        os.path.join(os.path.dirname(__file__), "src"),
+        os.path.dirname(__file__),
+    ]
+)
 
 wsgi = imp.load_source("wsgi", "src/run.py")
 application = wsgi.app
