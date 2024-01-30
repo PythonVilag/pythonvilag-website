f8a91d7b85e6adde0be5bf14ca3095bd3511060a
Daniel Mizsak <mizsak.dani@gmail.com>
2024-01-30 21:58:05 +0100

    Update gunicorn configurations

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,16 +4,6 @@ Deploying the application with Gunicorn.
 @author "Daniel Mizsak" <info@pythonvilag.hu>
 """
 
-import sys
-from pathlib import Path
-
-sys.path.extend(
-    [
-        str(Path(__file__).parent / "src"),
-        str(Path(__file__).parent),
-    ],
-)
-
 bind = "0.0.0.0:5000"
 worker_tmp_dir = "/dev/shm"  # noqa: S108
 workers = 2
